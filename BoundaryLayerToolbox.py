3d06b00f22ceaf054a68b3692d80a5329574093f
Claudio Pierard <cl.pierard@gmail.com>
2017-07-20 09:56:05 -0500

    Mejoro el módulo BLtoolbox

--- BoundaryLayerToolbox.py
+++ BoundaryLayerToolbox.py
@@ -1,57 +1,72 @@
-'''
+import scipy.interpolate as interpolate
+import numpy as np
 
-    `XLON`, `XLAT`, ...  van con su dimensión temporal.
-    `U` no va con su dimensión temporal, sólo las dimensiones espaciales correspondientes a un tiempo.
-    `ej: U[1, :, :, :]`
+def interpolate_velocity(XLONG, XLAT, XLONG_U, XLAT_U, U, uu = True, Kind = 'linear'):
 
-'''
+    '''
+    interpolate_velocity(XLONG, XLAT, XLONG_U, XLAT_U, U, uu = True, Kind = 'linear')
 
+        `XLON`, `XLAT`, `XLONG_U`, `XLAT_U`...  van con su dimensión temporal.
+        `U` va con su dimensión temporal.
+        `uu` define si se trata de velocidad zonal o meridional, es decir si se está interpolando 'u', uu = True.
+        `Kind`, tipo de interpolación. Por default es linear.
 
-def interpolate_velocity(XLONG, XLAT, XLONG_U, XLAT_U, U, u = True, Kind = 'linear'):
+        ej:
+            interpolate_velocity(xlong, xlat, xlong_v, xlat_v, v, uu = False)
+
+    '''
 
     x_old = XLONG_U[0, 1, :]
     y_old = XLAT_U[0, :, 1]
     x_new = XLONG[0, 1, :]
     y_new = XLAT[0, :, 1]
 
-    if u == True:
-        u_shape = (U.shape[0], U.shape[1], U.shape[2]-1)
+    if uu == True:
+        u_shape = (U.shape[0], U.shape[1], U.shape[2], U.shape[3]-1)
 
     else:
-        u_shape = (U.shape[0], U.shape[1] - 1, U.shape[2])
+        u_shape = (U.shape[0], U.shape[1] ,U.shape[2] - 1, U.shape[3])
 
 
     U_out = np.zeros(u_shape, dtype='float32')
     #U_out = np.zeros(u_shape, dtype='float32')
 
-    for h in range(0, U.shape[0]):
+    for t in range(0, U.shape[0]):
+        for h in range(0, U.shape[1]):
 
-        f = interpolate.interp2d(x_old, y_old, U[h, :, :], kind = Kind)
-        U_out[h, :, :] = f(x_new, y_new)
+            f = interpolate.interp2d(x_old, y_old, U[t, h, :, :], kind = Kind)
+            U_out[t, h, :, :] = f(x_new, y_new)
 
     return U_out
 
-'''
+def compute_height(PH, PHB):
+
+    '''
     compute_height(PH, PHB)
 
-    PH y PHB deben ser prporcionados como una matriz de tres dimensiones es decir hay especificar su variable temporal.
-    Por ejemplo: Para el tiempo t = 0, se debe introducir PH[0, :, :, :] y PHB[0, :, :, :].
-'''
+        PH y PHB deben ser prporcionados como una matriz de 4 dimensiones es decir no hay especificar su variable temporal.
 
-def compute_height(PH, PBH):
+    '''
 
     g = 9.8
-    z_shape = (PH.shape[0]-1, PH.shape[1], PH.shape[2])
+    z_shape = (PH.shape[0], PH.shape[1]-1, PH.shape[2], PH.shape[3])
     Z = np.zeros(z_shape, dtype='float32')
 
+    for t in range(0, PH.shape[0]):
+        for h in range(0, PH.shape[1]-1):
 
-    for h in range(0, PH.shape[0]-1):
-
-        Z[h, :, :] = ((PH[h, :, :] + PBH[h, :, :]) + (PH[h+1, :, :] + PBH[h+1, :, :]))/g
+            Z[t, h, :, :] = ((PH[t, h, :, :] + PHB[t, h, :, :]) + (PH[t, h+1, :, :] + PHB[t, h+1, :, :]))/g
 
     return Z
 
 def compute_Richardson(T_s, θ, Z, u, v):
+
+    '''
+        compute_Richardson(T_s, θ, Z, u, v):
+            Especificar tiempo y localización.
+
+    '''
+
     g = 9.8
     Ri = np.zeros(Z.shape, dtype='float32')
 
@@ -61,5 +76,22 @@ def compute_Richardson(T_s, θ, Z, u, v):
         Vd = np.sqrt(u[i-1]**2 + v[i-1]**2)
 
         Ri[i] = g / T_s * ((θ[i+1] - θ[i-1])*(Z[i+1] - Z[i-1]))/(Vu - Vd)**2
+
+    return Ri
+
+
+def compute_Richardson_2(θ, Z, u, v):
+
+    '''
+        compute_Richardson_2(θ, Z, u, v):
+            Especificar tiempo y localización
+    '''
+
+    g = 9.8
+    Ri = np.zeros(Z.shape, dtype='float32')
+
+    for i in range(0,len(θ)-1):
+
+        Ri[i] = (g*(θ[i] - θ[0])*(Z[i] - Z[0]))/(θ[i] * (u[i]**2 + v[i]**2))
 
     return Ri
